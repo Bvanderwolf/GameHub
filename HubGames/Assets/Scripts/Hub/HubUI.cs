cd4abd37cdc9cb6535e3809604f8f1b9d110fe16
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-01-19 22:06:01 +0100

    Updated the hub buttons to be video players

---
+++ HubGames/Assets/Scripts/Hub/HubUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class HubUI : MonoBehaviour
+{
+    [SerializeField] private GameObject[] hubButtons;
+
+    // Start is called before the first frame update
+    private void Start ()
+    {
+        foreach (GameObject button in hubButtons)
+        {
+            StartCoroutine(PlayVideo(
+                button.GetComponent<VideoPlayer>(),
+                button.GetComponent<RawImage>()));
+        }
+    }
+
+    private IEnumerator PlayVideo (VideoPlayer videoPlayer, RawImage image)
+    {
+        videoPlayer.Prepare();
+        yield return new WaitUntil(() => videoPlayer.isPrepared);
+        image.texture = videoPlayer.texture;
+        videoPlayer.Play();
+
+        float currentLerpTime = 0;
+        while (currentLerpTime != 1f)
+        {
+            currentLerpTime += Time.deltaTime;
+            if (currentLerpTime > 1f) currentLerpTime = 1f;
+            image.color = new Color(1, 1, 1, currentLerpTime * 1);
+            yield return null;
+        }
+    }
+
+    private void OnDestroy ()
+    {
+        StopAllCoroutines();
+    }
+}
